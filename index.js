9b99f68029aee6934cfba06aaa5598e5e06f7f17
Liviries <liviriez@gmail.com>
2025-01-11 15:57:09 +0200

    beta

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
-
-
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 let firstCard = 13
 let secondCard = 8
@@ -9,22 +10,28 @@ let message = ''
 let bJ = ''
 let alive = ''
 
-if (sum <= 20) {
-    message = 'Do u want draw a new card?'
-    bJ = false
-    alive = true
-}
-else if (sum === 21) {
-    message = 'Win!'
-    bJ = true
-    alive = true
-}
-else{
-    message = 'lose('
-    bJ = false
-    alive = false
+function startGame() {
+
+    if (sum <= 20) {
+        message = 'Do u want draw a new card?'
+        bJ = false
+        alive = true
+    }  else if (sum === 21) {
+        message = 'Win!'
+        bJ = true
+        alive = true
+    }  else{
+        message = 'lose('
+        bJ = false
+        alive = false
+    }
+
+    messageEl.textContent = message
+    cardsEl.textContent = 'Cards: ' + firstCard + ', ' + secondCard
+    sumEl.textContent = 'Sum: ' + sum
+
+
+    console.log('BlackJack?', bJ)
+    console.log('Alive?', alive)
 }
 
-console.log(message)
-console.log('BlackJack?', bJ)
-console.log('Alive?', alive)
