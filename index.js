c2204690053c33dc94b6034eafd3bfc95ab56560
Liviries <liviriez@gmail.com>
2025-01-12 22:00:50 +0200

    v0.4

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 
+
+
+
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard
-let message = ''
-let bJ = ''
-let alive = ''
 
-function getRandomCard(){
- return Math.floor(Math.random() * 10) + 2
+let cards = [];
+let sum = 0
+let message = ''
+let bJ = false
+let isAlive = true
+
+console.log(cards)
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber === 1) {
+        return 11
+
+    } else if (randomNumber > 10) {
+        return 10
+
+    } else{
+        return randomNumber
+    }
 }
 
+
 function startGame() {
+    isAlive = true
+    bJ = false
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+
+    sum += firstCard + secondCard
+    cards.push([firstCard, secondCard])
+
     renderGame()
 }
 
@@ -23,16 +47,14 @@ function renderGame() {
 
     if (sum <= 20) {
         message = 'Do u want to draw a new card?'
-        bJ = false
-        alive = true
+
     }  else if (sum === 21) {
         message = 'U won, blackjack!'
         bJ = true
-        alive = true
+
     }  else{
         message = 'U lost('
-        bJ = false
-        alive = false
+        isAlive = false
     }
 
     messageEl.textContent = message
@@ -46,13 +68,15 @@ function renderGame() {
 
 
     console.log('BlackJack?', bJ)
-    console.log('Alive?', alive)
+    console.log('isAlive?', isAlive)
 }
 
 function newCard() {
     let newCard = getRandomCard();
+
     sum += newCard
     cards.push(newCard)
+
     renderGame()
 }
 
